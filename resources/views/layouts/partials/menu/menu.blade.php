07f4e7fe88d1dca10c1a0c462a9ed45e20fbb272
stygiansabyss <stygian.warlock.v2@gmail.com>
2017-08-07 13:33:46 -0500

    Making menu drop down a click event instead of hover

--- resources/views/layouts/partials/menu/menu.blade.php
+++ resources/views/layouts/partials/menu/menu.blade.php
@@ -1,7 +1,7 @@
 @if ($item->isDropDown() && $item->hasLinks())
   <li class="{{ $item->active ? 'uk-active' : '' }}">
     <a href="#">{{ $item->name }}<i class="fa fa-fw fa-caret-down"></i></a>
-    <div class="uk-navbar-dropdown">
+    <div uk-dropdown="mode: click">
       <ul class="uk-nav uk-navbar-dropdown-nav">
         @each('layouts.partials.menu.sub-menu', $item->links, 'item')
       </ul>
