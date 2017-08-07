727499e016ed3f838bea846b80940d64b71a6351
stygiansabyss <stygian.warlock.v2@gmail.com>
2017-08-07 15:19:13 -0500

    Adding user factory

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -16,9 +16,18 @@ $factory->define(App\Models\User::class, function (Faker\Generator $faker) {
     static $password;
 
     return [
-        'name'           => $faker->name,
         'email'          => $faker->unique()->safeEmail,
-        'password'       => $password ?: $password = bcrypt('secret'),
+        'password'       => $password ?: $password = bcrypt(\Illuminate\Support\Str::random(10)),
+        'status_id'      => rand(1, 3),
         'remember_token' => str_random(10),
+    ];
+});
+
+$factory->define(JumpGate\Users\Models\User\Detail::class, function (Faker\Generator $faker) {
+    return [
+        'first_name'   => $faker->firstName,
+        'middle_name'  => $faker->firstName,
+        'last_name'    => $faker->lastName,
+        'display_name' => $faker->userName,
     ];
 });
