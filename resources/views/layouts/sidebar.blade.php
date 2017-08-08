81a3d218244c606e825741df858ae602c670c363
stygiansabyss <stygian.warlock.v2@gmail.com>
2017-08-08 07:50:36 -0500

    Adding admin menu check to sidebar by default

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -19,7 +19,13 @@
           </div>
           <div class="uk-grid-collapse" uk-grid uk-height-viewport="expand: true">
             <div class="uk-width-1-6 uk-background-gray-dark uk-sidebar">
-              @yield('sidebar')
+              @section('sidebar')
+                @if (Menu::exists('adminMenu') && Menu::hasLinks('adminMenu'))
+                  @section('sidebar')
+                    @include('layouts.partials.sidebar-menu', ['menu' => Menu::render('adminMenu')])
+                  @endsection
+                @endif
+              @show
             </div>
             <div class="uk-width-5-6">
               @if (isset($content))
